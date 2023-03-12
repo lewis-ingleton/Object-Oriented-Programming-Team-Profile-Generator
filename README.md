1bbc40a02ffb248f1844cc8404ec3b3c36a5e3a8
Lewis Ingleton <lewis.ingleton1@.com>
2023-03-12 21:49:19 +0000

    html file generated

--- README.md
+++ README.md
@@ -4,6 +4,9 @@ This team profile generator uses node.js and the Inquirer package so that a user
 ## User story 
 As a manager, I want a quick and efficient way to create a HTML document that displays key information for employees on my team. 
 
+## Video example
+I have uploaded an example run through in the 'video-example' folder. You will find the generated html file in the 'output' folder.
+
 ## Usage
 To run this application, type 'node index.js' in the command line to run a number of prompts that will ask for information about you, as a manager, and your team. 
 
@@ -14,6 +17,3 @@ https://www.npmjs.com/package/inquirer
 
 ## Tests 
 I have used Jest to test the initial starter code. All tests in the test.js files have been fixed. 
-
-## Project status
-At time of writing (7 March 2023) this project is unfinished.
--- starter/index.js
+++ starter/index.js
@@ -1,124 +1,145 @@
 const Manager = require("./lib/Manager");
 const Engineer = require("./lib/Engineer");
 const Intern = require("./lib/Intern");
+
+
 const inquirer = require("inquirer");
 const path = require("path");
 const fs = require("fs");
+
 const generateTeam = require("./src/page-template")
 
 const OUTPUT_DIR = path.resolve(__dirname, "output");
 const outputPath = path.join(OUTPUT_DIR, "team.html");
 
-const render = require("./src/page-template.js");
 
 const newStaffArr = [];
 
-// TODO: Write Code to gather information about the development team members, and render the HTML file.
-
-
 // Team manager (user) prompts 
-inquirer
-    .prompt([
+
+const managerInput = () => {
+    inquirer
+        .prompt([
+            {
+                type: 'input',
+                message: 'Enter managers name',
+                name: 'name',
+            },
+            {
+                type: 'input',
+                message: 'Enter managers ID',
+                name: 'id',
+            },
+            {
+                type: 'input',
+                message: 'Enter managers email',
+                name: 'email',
+            },
+            {
+                type: 'input',
+                message: 'Enter managers office number',
+                name: 'number',
+            },
+        ]).then(({ name, id, email, number }) => {
+            const manager = new Manager(name, id, email, number);
+            newStaffArr.push(manager)
+            console.log(newStaffArr)
+            addTeamMembers()
+        })
+    }
+
+managerInput()
+
+const createEngineer = () => {
+    inquirer
+        .prompt([
         {
-            type: 'confirm',
-            message: 'Are you a manager?',
-            name: 'managerConfirm',
+            type: 'input',
+            message: 'Enter engineers name',
+            name: 'engineerName',
         },
-    ])
+        {
+            type: 'input',
+            message: 'Enter engineers ID',
+            name: 'engineerID',
+        },
+        {
+            type: 'input',
+            message: 'Enter engineers email',
+            name: 'engineerEmail',
+        },
+        {
+            type: 'input',
+            message: 'Enter engineers GitHub username',
+            name: 'engineerGithub',
+        },
+        ]).then(({ engineerName, engineerID, engineerEmail, engineerGithub }) => {
+            const engineer = new Engineer (engineerName, engineerID, engineerEmail, engineerGithub);
+            newStaffArr.push(engineer)
+            console.log(newStaffArr)
+            addTeamMembers()
+        })
 
-
-    .then((response) => {
-        if (response.managerConfirm === false) {
-            console.log('You must be a manager to use this terminal.')
-        } else {
-            inquirer.prompt([
-                {
-                    type: 'input',
-                    message: 'What is your name?',
-                    name: 'name',
-                },
-                {
-                    type: 'input',
-                    message: 'What is your ID?',
-                    name: 'id',
-                },
-                {
-                    type: 'input',
-                    message: 'What is your email?',
-                    name: 'email',
-                },
-                {
-                    type: 'input',
-                    message: 'What is the office number?',
-                    name: 'number',
-                },
-                {
-                    type: 'list',
-                    message: 'Would you like to add new members to the team?',
-                    name: 'addTeam',
-                    choices: ['Engineer', 'Intern', 'Finish building the team']
-                },
-            ])
-            
-                .then((response) => {
-                    if (response.addTeam == 'Engineer') {
-                        inquirer.prompt([
-                            {
-                                type: 'input',
-                                message: 'What is engineers name?',
-                                name: 'engineerName',
-                            },
-                            {
-                                type: 'input',
-                                message: 'What is engineers ID?',
-                                name: 'engineerID',
-                            },
-                            {
-                                type: 'input',
-                                message: 'What is engineers email?',
-                                name: 'engineerEmail',
-                            },
-                            {
-                                type: 'input',
-                                message: 'What is enginners GitHub username?',
-                                name: 'engineerGithub',
-                            },
-                        ])
-
-                    } else if (response.addTeam == 'Intern') {
-                        inquirer.prompt([
-                            {
-                                type: 'input',
-                                message: 'What is interns name?',
-                                name: 'internName',
-                            },
-                            {
-                                type: 'input',
-                                message: 'What is interns ID?',
-                                name: 'internID',
-                            },
-                            {
-                                type: 'input',
-                                message: 'What is interns email?',
-                                name: 'internEmail',
-                            },
-                            {
-                                type: 'input',
-                                message: 'What is the interns school?',
-                                name: 'internSchool',
-                            }
-                        ])
-                    } else if (response.addTeam === 'Finsh building the team') {
-                        return ((response) => {
-                            const HTMLgeneration = generateTeam(response);
-                            console.log(response)
-                            fs.writeFile(`team.html`, HTMLgeneration, (error) => error ? console.error(error) : console.log(`Success`)
-                            );
-                        })
-                    }
-                })
+    }
+    
+    const createIntern = () => {
+        inquirer
+            .prompt([
+            {
+                type: 'input',
+                message: 'Enter interns name',
+                name: 'internName',
+            },
+            {
+                type: 'input',
+                message: 'Enter interns ID',
+                name: 'internID',
+            },
+            {
+                type: 'input',
+                message: 'Enter interns email',
+                name: 'internEmail',
+            },
+            {
+                type: 'input',
+                message: 'Enter interns school',
+                name: 'internSchool',
+            },
+            ]).then(({ internName, internID, internEmail, internSchool }) => {
+                const intern = new Intern (internName, internID, internEmail, internSchool);
+                newStaffArr.push(intern)
+                console.log(newStaffArr)
+                addTeamMembers()
+            })
+    
         }
-    })
 
+addTeamMembers = () => {
+    inquirer
+    .prompt(
+        {
+            type: 'list',
+            message: 'Would you like to add new members to the team?',
+            name: 'addTeam',
+            choices: ['Engineer', 'Intern', 'Finish building the team']
+        },
+    ).then((answers) => {
+        switch (answers.addTeam) {
+            case 'Engineer': {
+                createEngineer();
+                break;
+            }
+            case 'Intern': {
+                createIntern();
+                break;
+            }
+            default: generateHTML();
+            break;
+        };
 
-    // if user chooses 'Finish building team' >>> export file to html 
+    });
+};
+        
+let generateHTML = () => {
+    fs.writeFileSync(outputPath, generateTeam(newStaffArr), 'utf-8')
+}
